9c9801350beb45b84fb298a4942fd052ba498cd7
Tim Lau <tlau9195@gmail.com>
2014-06-30 10:50:06 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,16 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Tim Lau on 6/29/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+@end
